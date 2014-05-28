a38b2350fe8b9253f6f619e9bb5f2b3a9f473236
Sameer Rahmani <lxsameer@gnu.org>
2014-05-28 14:19:55 +0430

    deploy script improved

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+require 'mina/rbenv'
+require "mina/rsync"
 # require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 # Basic settings:
@@ -10,25 +11,28 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
+set :domain, 'shop.demo.dev.yellowen.ir'
+set :deploy_to, '/home/demo/shop'
+set :repository, 'git://github.com/lxsameer/Bazaar.git'
 set :branch, 'master'
 
+set :rsync_options, %w[--recursive --delete --delete-excluded --exclude .git*]
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, ['log']
 
 # Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
+set :user, 'demo'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
+set :term_mode, :system
+set :forward_agent, true
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
   # If you're using rbenv, use this to load the rbenv environment.
   # Be sure to commit your .rbenv-version to your repository.
-  # invoke :'rbenv:load'
+  invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
   # invoke :'rvm:use[ruby-1.9.3-p125@default]'
@@ -48,17 +52,27 @@ task :setup => :environment do
   queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
 end
 
+task :precompile do
+  Dir.chdir settings.rsync_stage do
+    system "cp -rv ../../public/assets/ ./public/"
+  end
+end
+
+task "rsync:stage" do
+  invoke "precompile"
+end
+
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
-    invoke :'git:clone'
+    #invoke :'git:clone'
+    invoke "rsync:deploy"
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
-
+    #invoke :'assets_precompile'
     to :launch do
       queue "touch #{deploy_to}/tmp/restart.txt"
     end
@@ -71,4 +85,3 @@ end
 #  - http://nadarei.co/mina/tasks
 #  - http://nadarei.co/mina/settings
 #  - http://nadarei.co/mina/helpers
-
